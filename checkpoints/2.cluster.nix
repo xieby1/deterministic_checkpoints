edfde95f5890d04d1d58ed1135d70d1badd498e7
xieby1 <xieby1@outlook.com>
2024-09-03 11:12:48 +0800

    add 2.cluster

---
+++ checkpoints/2.cluster.nix
@@ -0,0 +1,19 @@
+let
+  name = "2.cluster.nix";
+  pkgs = import <nixpkgs> {};
+  simpoint = import ../simpoint;
+  stage1_profiling = import ./1.profiling.nix;
+in pkgs.runCommand name {} (''
+  mkdir -p $out
+'' + (builtins.toString [
+  "${simpoint}/bin/simpoint"
+  "-loadFVFile ${stage1_profiling}/simpoint_bbv.gz"
+  "-saveSimpoints $out/simpoints0"
+  "-saveSimpointWeights $out/weights0"
+  "-inputVectorsGzipped"
+  "-maxK 5"
+  "-numInitSeeds 2"
+  "-iters 1000"
+  "-seedkm 610829"
+  "-seedproj 829610"
+]))
