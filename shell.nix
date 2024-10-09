f0bc4d55066357afadafbe2f953d044682c89772
Yan Yue <1131531947@qq.com>
2024-10-09 11:22:05 +0800

    update README: add reference, config.nix usage

--- shell.nix
+++ shell.nix
@@ -4,11 +4,14 @@ let
   h_content = builtins.toFile "h_content" ''
     # ${pkgs.lib.toUpper "${name} usage tips"}
 
-    * Set SPEC CPU 2006 source code: edit `spec2006/default.nix`: `srcs = [...]`
-    * Set input size: edit `spec2006/default.nix`: `size = xxx` (default input is ref)
+    * Set SPEC CPU 2006 source code: edit `config.nix`: `spec2006_path = [...]`
+    * Set input size: edit `config.nix`: `size = xxx` (default input is ref)
+    * Change other configs in `config.nix`
     * Generate the checkpoints of all testCases into `result/`: `nom-build -A checkpoints`
     * Generate the checkpoints of a specific <testCase> into `result/`: `nom-build -A 'checkpoints."<testCase>"'`
       * E.g.: `nom-build -A 'checkpoints."403.gcc"'`
+    * Running nom-build without parameters will generate results-* directory, used by dump_result.py
+      * E.g.: `nom-build`
   '';
   _h_ = pkgs.writeShellScriptBin "h" ''
     ${pkgs.glow}/bin/glow ${h_content}
