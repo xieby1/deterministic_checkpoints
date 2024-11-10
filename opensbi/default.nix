b1e886bbb4071f8980e09a48ccab6132dd8d98bc
xieby1 <xieby1@outlook.com>
2024-11-10 16:22:56 +0800

    decouple: opensbi

--- opensbi/default.nix
+++ opensbi/default.nix
@@ -1,16 +1,13 @@
 { stdenv
+, lib
 , python3
 
-, testCase
 , riscv64-cc
 , linux-image
 , dts
 , opensbi-common-build
-}:
-let
-  name = "opensbi-${testCase}";
-in stdenv.mkDerivation {
-  inherit name;
+}: stdenv.mkDerivation {
+  name = "${lib.removeSuffix ".linux" linux-image.name}.opensbi";
 
   src = opensbi-common-build;
 
@@ -23,7 +20,7 @@ in stdenv.mkDerivation {
     "CROSS_COMPILE=riscv64-unknown-linux-gnu-"
     "PLATFORM=generic"
     "FW_FDT_PATH=${dts}/xiangshan.dtb"
-    "FW_PAYLOAD_PATH=${linux-image}/arch/riscv/boot/Image.${testCase}"
+    "FW_PAYLOAD_PATH=${linux-image}"
   ];
   buildPhase = ''
     patchShebangs .
@@ -46,7 +43,7 @@ in stdenv.mkDerivation {
     # Calculate the FW_PAYLOAD_FDT_OFFSET
     ALIGN=0x200000
     FW_PAYLOAD_OFFSET=0x100000
-    IMAGE_SIZE=$(ls -l ${linux-image}/arch/riscv/boot/Image.${testCase} | awk '{print $5}')
+    IMAGE_SIZE=$(ls -l ${linux-image} | awk '{print $5}')
     IMAGE_END=$((FW_PAYLOAD_OFFSET + IMAGE_SIZE))
     IMAGE_END_ALIGNED=$(( (IMAGE_END + ALIGN-1) & ~(ALIGN-1) ))
     IMAGE_END_ALIGNED_HEX=$(printf "0x%x" $IMAGE_END_ALIGNED)
@@ -58,7 +55,6 @@ in stdenv.mkDerivation {
   '';
 
   installPhase = ''
-    mkdir -p $out
-    mv build/platform/generic/firmware/fw_payload.bin $out/fw_payload.${testCase}.bin
+    cp build/platform/generic/firmware/fw_payload.bin $out
   '';
 }
