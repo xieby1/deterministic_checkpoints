79473c0760d15fddb2af3af83c45716ea5cc2ce6
xieby1 <xieby1@outlook.com>
2024-11-10 15:58:26 +0800

    decouple: initramfs

--- linux/initramfs/default.nix
+++ linux/initramfs/default.nix
@@ -3,21 +3,12 @@
 , cpio
 , initramfs_base
 , initramfs_overlays
-, spec2006
-}:
-let
-  name = "spec2006.cpio";
-in runCommand name {} ''
-  mkdir -p $out
-
-  for WORK_DIR in ${spec2006}/[0-9][0-9][0-9].*; do
-    TESTCASE_NAME=''${WORK_DIR##*/}
-    cp ${initramfs_base}/init.cpio $out/$TESTCASE_NAME.cpio
-    chmod +w $out/$TESTCASE_NAME.cpio
-
-    cd $WORK_DIR
-    find . | sort -n | ${cpio}/bin/cpio --reproducible -H newc -oAF $out/$TESTCASE_NAME.cpio
-    cd ${initramfs_overlays}
-    find . | sort -n | ${cpio}/bin/cpio --reproducible -H newc -oAF $out/$TESTCASE_NAME.cpio
-  done
+, benchmark
+}: runCommand "${benchmark.name}.cpio" {} ''
+  cp ${initramfs_base}/init.cpio $out
+  chmod +w $out
+  cd ${benchmark}
+  find . | sort -n | ${cpio}/bin/cpio --reproducible -H newc -oAF $out
+  cd ${initramfs_overlays}
+  find . | sort -n | ${cpio}/bin/cpio --reproducible -H newc -oAF $out
 ''
