702194d9c2014ba0f8af18bf95c080026ccc1159
xieby1 <xieby1@outlook.com>
2024-11-10 17:21:11 +0800

    decouple: add cptBuilder

---
+++ cptBuilder/default.nix
@@ -0,0 +1,23 @@
+{ pkgs
+, gcpt-bin
+}: let
+  riscv64-cc = pkgs.pkgsCross.riscv64.stdenv.cc;
+  # TODO: move folders to cptBuilder/
+  qemu = pkgs.callPackage ../qemu {};
+  nemu = pkgs.callPackage ../nemu {inherit riscv64-cc;};
+  stage1-profiling = pkgs.callPackage ../checkpoints/1.profiling.nix {
+    inherit qemu nemu gcpt-bin;
+  };
+  simpoint = pkgs.callPackage ../simpoint {};
+  stage2-cluster = pkgs.callPackage ../checkpoints/2.cluster.nix {
+    inherit simpoint stage1-profiling;
+  };
+  stage3-checkpoint = pkgs.callPackage ../checkpoints/3.checkpoint.nix {
+    inherit qemu nemu gcpt-bin stage2-cluster;
+  };
+in stage3-checkpoint.overrideAttrs (old: {
+  passthru = {
+    inherit riscv64-cc qemu nemu simpoint;
+    inherit stage1-profiling stage2-cluster stage3-checkpoint;
+  };
+})
