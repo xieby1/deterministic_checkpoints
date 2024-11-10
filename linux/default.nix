1259202be8fca57395d395a3b0b000a1f28ce735
xieby1 <xieby1@outlook.com>
2024-11-10 16:16:57 +0800

    decouple: linux

--- linux/default.nix
+++ linux/default.nix
@@ -1,17 +1,15 @@
 { stdenv
+, lib
 , bc
 , flex
 , bison
 
-, testCase
 , riscv64-cc
 , initramfs
+# TODO: use overlayfs to reduce disk usage
 , linux-common-build
-}:
-let
-  name = "linux-${testCase}";
-in stdenv.mkDerivation {
-  inherit name;
+}: stdenv.mkDerivation {
+  name = "${lib.removeSuffix ".cpio" initramfs.name}.linux";
   src = linux-common-build;
   buildInputs = [
     bc
@@ -25,18 +23,16 @@ in stdenv.mkDerivation {
     export RISCV_ROOTFS_HOME=$(realpath ../riscv-rootfs/)
     export CROSS_COMPILE=riscv64-unknown-linux-gnu-
 
-    # Prepare testCase config
-    TESTCASE_DEFCONFIG=arch/riscv/configs/xiangshan_${testCase}_defconfig
+    # Prepare benchmark config
+    TESTCASE_DEFCONFIG=arch/riscv/configs/xiangshan_benchmark_defconfig
     cat arch/riscv/configs/xiangshan_defconfig > $TESTCASE_DEFCONFIG
-    echo CONFIG_INITRAMFS_SOURCE=\"${initramfs}/${testCase}.cpio\" >> $TESTCASE_DEFCONFIG
+    echo CONFIG_INITRAMFS_SOURCE=\"${initramfs}\" >> $TESTCASE_DEFCONFIG
 
     export KBUILD_BUILD_TIMESTAMP=@0
-    make xiangshan_${testCase}_defconfig
+    make xiangshan_benchmark_defconfig
     make -j $NIX_BUILD_CORES
-    mv arch/riscv/boot/Image arch/riscv/boot/Image.${testCase}
   '';
   installPhase = ''
-    mkdir -p $out/arch/riscv/boot/
-    cp arch/riscv/boot/Image.${testCase} $out/arch/riscv/boot/
+    cp arch/riscv/boot/Image $out
   '';
 }
