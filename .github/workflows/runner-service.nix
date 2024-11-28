abf10328c0ac1ee3de52b6b00b26ff68bc6696ee
xieby1 <xieby1@outlook.com>
2024-11-28 09:31:12 +0800

    github: runner: use github_token

--- .github/workflows/runner-service.nix
+++ .github/workflows/runner-service.nix
@@ -1,6 +1,19 @@
-# build example: `nix-build runner-service.nix --argstr spec2006src /nfs/share/home/spec2006.tar.gz`
+# Usage:
+#   1. build service: `nix-build runner-service.nix --argstr spec2006src <SPEC2006SRC> --argstr github_token <GITHUB_TOKEN>`
+#     * The GitHub token (valid for 366 days, limited by OpenXiangShan) is needed to
+#       retrieve the runner token (only valid for one hour, limited by GitHub).
+#       https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-a-repository
+#       > The fine-grained token must have the following permission set:
+#       > "Administration" repository permissions (write)
+#     * Generate the github token here:
+#       https://github.com/settings/tokens?type=beta
+#   2. Copy generate file `result` to ~/.config/systemd/user/${name}.service
+#   3. Start this service: `systemctl --user start ${name}.service
+#   *. Inspect the status of this service: `systemctl --user status ${name}.service`
+#   *. Inspect the output of this service: `journalctl --user -f -u ${name}.service`
 { pkgs ? import <nixpkgs> {}
 , spec2006src
+, github_token
 }: let
   name = "runner-deterload";
   runner = import (pkgs.fetchFromGitHub {
@@ -12,21 +25,29 @@
     inherit pkgs;
     podmanExtraOpts = ["-v ${spec2006src}:${spec2006src}:ro"];
   };
+  run = pkgs.writeShellScript "run" ''
+    resp=$(curl -L \
+      -X POST \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer ${github_token}" \
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      Https://api.github.com/repos/OpenXiangShan/Deterload/actions/runners/registration-token)
+    # https://unix.stackexchange.com/questions/13466/can-grep-output-only-specified-groupings-that-match
+    runner_token=$(echo $resp | grep -oP '"token":\s*"\K[^"]*')
+    ${runner}/bin/github-runner-nix \
+      --labels 'self-hosted,Linux,X64,nix' \
+      --ephemeral \
+      --url https://github.com/OpenXiangShan/Deterload \
+      --token $runner_token
+  '';
 in pkgs.writeTextFile {
   name = "${name}.service";
   text = ''
-    # Usage:
-    #   1. Copy this file to ~/.config/systemd/user/${name}.service
-    #   2. Replace [token](https://github.com/OpenXiangShan/Deterload/settings/actions/runners/new) below
-    #   3. Start this service: `systemctl --user start ${name}.service
-    #   *. Inspect the status of this service: `systemctl --user status ${name}.service`
-    #   *. Inspect the output of this service: `journalctl --user -f -u ${name}.service`
-
     [Install]
     WantedBy=default.target
 
     [Service]
-    ExecStart=${runner}/bin/github-runner-nix --labels 'self-hosted,Linux,X64,nix' --ephemeral --url https://github.com/OpenXiangShan/Deterload --token <REPLACE_YOUR_TOKEN_HERE>
+    ExecStart=${run}
     Restart=always
 
     [Unit]
