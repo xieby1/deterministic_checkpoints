d55ce73f4cf2f1b41d8ac9ac9d461396f6bb09ae
xieby1 <xieby1@outlook.com>
2024-12-08 11:58:36 +0800

    flat: move riscv64-* into riscv64-scope

--- default.nix
+++ default.nix
@@ -5,34 +5,33 @@
   , ...
 } @ args:
 let
-  riscv64-pkgs = pkgs.pkgsCross.riscv64;
-  # TODO: gcc14 have a bug to compile spec2006 & spec2017's xalan
-  #   * https://github.com/llvm/llvm-project/issues/109966
-  #   * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=116064
-  riscv64-stdenv = riscv64-pkgs.gcc13Stdenv;
-  riscv64-cc = riscv64-stdenv.cc;
-  riscv64-libc-static = riscv64-stdenv.cc.libc.static;
-  riscv64-fortran = riscv64-pkgs.wrapCCWith {
-    cc = riscv64-stdenv.cc.cc.override {
-      name = "gfortran";
-      langFortran = true;
-      langCC = false;
-      langC = false;
-      profiledCompiler = false;
-    };
-    # fixup wrapped prefix, which only appear if hostPlatform!=targetPlatform
-    #   for more details see <nixpkgs>/pkgs/build-support/cc-wrapper/default.nix
-    stdenvNoCC = riscv64-pkgs.stdenvNoCC.override {
-      hostPlatform = pkgs.stdenv.hostPlatform;
-    };
-    # Beginning from 24.05, wrapCCWith receive `runtimeShell`.
-    # If leave it empty, the default uses riscv64-pkgs.runtimeShell,
-    # thus executing the sheBang will throw error:
-    #   `cannot execute: required file not found`.
-    runtimeShell = pkgs.runtimeShell;
-  };
   riscv64-scope = pkgs.lib.makeScope pkgs.newScope (self: {
-    inherit riscv64-pkgs riscv64-stdenv riscv64-cc riscv64-libc-static riscv64-fortran;
+    riscv64-pkgs = pkgs.pkgsCross.riscv64;
+    # TODO: gcc14 have a bug to compile spec2006 & spec2017's xalan
+    #   * https://github.com/llvm/llvm-project/issues/109966
+    #   * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=116064
+    riscv64-stdenv = self.riscv64-pkgs.gcc13Stdenv;
+    riscv64-cc = self.riscv64-stdenv.cc;
+    riscv64-libc-static = self.riscv64-stdenv.cc.libc.static;
+    riscv64-fortran = self.riscv64-pkgs.wrapCCWith {
+      cc = self.riscv64-stdenv.cc.cc.override {
+        name = "gfortran";
+        langFortran = true;
+        langCC = false;
+        langC = false;
+        profiledCompiler = false;
+      };
+      # fixup wrapped prefix, which only appear if hostPlatform!=targetPlatform
+      #   for more details see <nixpkgs>/pkgs/build-support/cc-wrapper/default.nix
+      stdenvNoCC = self.riscv64-pkgs.stdenvNoCC.override {
+        hostPlatform = pkgs.stdenv.hostPlatform;
+      };
+      # Beginning from 24.05, wrapCCWith receive `runtimeShell`.
+      # If leave it empty, the default uses riscv64-pkgs.runtimeShell,
+      # thus executing the sheBang will throw error:
+      #   `cannot execute: required file not found`.
+      runtimeShell = pkgs.runtimeShell;
+    };
   });
   callPackage = riscv64-scope.callPackage;
   build = callPackage ./builders {};
