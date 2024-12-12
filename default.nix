6e22bfb8bcb405fafa49a2071601571068db664a
xieby1 <xieby1@outlook.com>
2024-12-12 16:43:16 +0800

    config: add spec2006-size

--- default.nix
+++ default.nix
@@ -2,8 +2,17 @@
     url = "https://github.com/NixOS/nixpkgs/archive/e8c38b73aeb218e27163376a2d617e61a2ad9b59.tar.gz";
     sha256 = "1n6gdjny8k5rwkxh6sp1iwg1y3ni1pm7lvh9sisifgjb18jdvzbm";
   }) {}
+
+#######################################################################################
+# Benchmarks Configuration
+#######################################################################################
 , spec2006-src ? throw "Please specify <spec2006-src> the path of spec2006, for example: /path/of/spec2006.tar.gz"
+, spec2006-size ? "ref"
 , enableVector ? false
+
+#######################################################################################
+# Builders Configuration
+#######################################################################################
 , cpt-maxK ? "30"
 # cpt-maxK for each benchmark
 # How to get the benchmark name:
@@ -18,6 +27,7 @@
 , cpt-simulator ? "qemu"
 , cpt-format ? "zstd"
 }:
+assert pkgs.lib.assertOneOf "spec2006-size" spec2006-size ["ref" "test"];
 assert pkgs.lib.assertOneOf "cpt-simulator" cpt-simulator ["qemu" "nemu"];
 assert pkgs.lib.assertOneOf "cpt-format" cpt-format ["gz" "zstd"];
 assert pkgs.lib.assertMsg (if cpt-simulator=="qemu" then cpt-format=="zstd" else true) "qemu only support cpt-format: zstd";
@@ -48,6 +58,7 @@ in raw.overrideScope (r-self: r-super: {
       benchmark = super.benchmark.override {
         inherit enableVector;
         src = spec2006-src;
+        size = spec2006-size;
       };
     })) bare;
   in bare-overrided // (r-super.tools.weave bare-overrided);
